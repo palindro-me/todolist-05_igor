110398b8520ea78b4e0075bafd3e8734337ffb51
Anna Kazak <anna.s.kazak@gmail.com>
2025-06-03 18:43:23 +0200

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/todolist-05_igor',
+  base: '/todolist-05_igor/',
   plugins: [react()],
 })
